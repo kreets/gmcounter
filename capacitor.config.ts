a59da20c88e42a6a7b3d82229392d06684cb194a
Arnold <superciberh@gmail.com>
2024-08-24 20:05:12 +0200

    IOS platform added

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'hu.devpartner.gmcounter',
+  appName: 'GM Score-counter',
+  webDir: 'dist'
+};
+
+export default config;
